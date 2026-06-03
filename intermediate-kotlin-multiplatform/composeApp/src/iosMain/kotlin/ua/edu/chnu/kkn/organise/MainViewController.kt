307bc3f762c9e460339b01496542646973fd4afb
Софія Загурська <sooonnnyyyaaa1@gmail.com>
2026-06-03 12:27:37 +0300

    [1] added about screen

---
+++ intermediate-kotlin-multiplatform/composeApp/src/iosMain/kotlin/ua/edu/chnu/kkn/organise/MainViewController.kt
@@ -0,0 +1,6 @@
+package ua.edu.chnu.kkn.organise
+
+import androidx.compose.ui.window.ComposeUIViewController
+import ua.edu.chnu.kkn.organise.ui.root.AppScaffold
+
+fun MainViewController() = ComposeUIViewController { AppScaffold() }
---
+++ intermediate-kotlin-multiplatform/composeApp/src/webMain/kotlin/ua/edu/chnu/kkn/organise/main.kt
@@ -0,0 +1,11 @@
+package ua.edu.chnu.kkn.organise
+
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.window.ComposeViewport
+
+@OptIn(ExperimentalComposeUiApi::class)
+fun main() {
+    ComposeViewport {
+        App()
+    }
+}
